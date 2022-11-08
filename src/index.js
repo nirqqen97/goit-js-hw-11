66172d4ed426700ca196a6b582d1e5c90474f618
nirqqen97 <93587073+nirqqen97@users.noreply.github.com>
2022-11-08 13:46:00 +0200

    trying to fix

--- src/index.js
+++ src/index.js
@@ -5,14 +5,6 @@ import { Notify } from 'notiflix/build/notiflix-notify-aio';
 import SimpleLightbox from "simplelightbox";
 import 'simplelightbox/dist/simple-lightbox.min.css';
 import './css/styles.css';
-
-
-// const smth = document.querySelector('.qwe')
-// smth.addEventListener('click', clickCH);
-// function clickCH(e) {
-
-//     refs.loadMoreRef.classList.remove('is-hidden')
-// }
 const newApiServive = new NewApiServive();
 let infoTotalHIts = 40
 
@@ -21,24 +13,25 @@ async function onSubmit(e) {
     e.preventDefault();
     resetHTML();
     infoTotalHIts = 40;
-    newApiServive.query = e.currentTarget.elements.searchQuery.value;
-    const data = await newApiServive.getPhotos()
-    Notify.info(`Hooray! We found ${data.totalHits} images.`)
-   if (data.hits.length === 0) {
-       Notify.failure("Sorry, there are no images matching your search query. Please try again.")
-       return
-   }
-  
-    const check = renderMarkUp(data.hits)
-    const simpleLightbox = new SimpleLightbox('.gallery-item');
-    simpleLightbox.refresh();
-    if (data.hits.length < 40) {
-     return 
+    try {
+        newApiServive.query = e.currentTarget.elements.searchQuery.value;
+        const data = await newApiServive.getPhotos()
+        Notify.info(`Hooray! We found ${data.totalHits} images.`)
+       if (data.hits.length === 0) {
+           Notify.failure("Sorry, there are no images matching your search query. Please try again.")
+           return
+       } 
+       const check = renderMarkUp(data.hits)
+       const simpleLightbox = new SimpleLightbox('.gallery-item');
+       simpleLightbox.refresh();
+       if (data.hits.length < 40) {
+        return 
+       }
+       refs.loadMoreRef.classList.remove('is-hidden')
+    } catch (error) {
+        Notify.failure('error')
     }
-    refs.loadMoreRef.classList.remove('is-hidden')
 }
-
-
 function renderMarkUp(data) {
     const markUp = createMarkUp(data)
  refs.galleryRef.insertAdjacentHTML('beforeend', markUp)
@@ -47,16 +40,22 @@ refs.loadMoreRef.addEventListener('click',onLoadMore)
 
 
 async function onLoadMore() {
-    const data = await newApiServive.onLoadMore()
-    const check = renderMarkUp(data.hits)
-    const simpleLightbox = new SimpleLightbox('.gallery-item');
-    simpleLightbox.refresh();
-    infoTotalHIts += 40;
-   if (data.totalHits <= infoTotalHIts) {
-      Notify.failure("We're sorry, but you've reached the end of search results.")
-      refs.loadMoreRef.classList.add('is-hidden')
-       
-   }
+    try {
+        const data = await newApiServive.onLoadMore()
+        const check = renderMarkUp(data.hits)
+        const simpleLightbox = new SimpleLightbox('.gallery-item');
+        simpleLightbox.refresh();
+        infoTotalHIts += 40;
+       if (data.totalHits <= infoTotalHIts) {
+          Notify.failure("We're sorry, but you've reached the end of search results.")
+          refs.loadMoreRef.classList.add('is-hidden')
+       }
+        
+    } catch (error) {
+    Notify.failure("error")
+        
+    }
+  
 }
 function resetHTML() {
     refs.galleryRef.innerHTML = '';
